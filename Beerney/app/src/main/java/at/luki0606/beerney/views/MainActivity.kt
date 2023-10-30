beb26029335fccc34aaff6a5b6c4f133df5d3585
06luki06 <lukas.riegler2000@gmail.com>
2023-10-30 08:34:26 +0100

    Try to get current gps position - currently not working

--- Beerney/app/src/main/java/at/luki0606/beerney/views/MainActivity.kt
+++ Beerney/app/src/main/java/at/luki0606/beerney/views/MainActivity.kt
@@ -1,6 +1,14 @@
 package at.luki0606.beerney.views
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -15,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import at.luki0606.beerney.ui.theme.Alabaster
 import at.luki0606.beerney.ui.theme.BeerneyTheme
 import at.luki0606.beerney.viewModels.beerList.BeerListViewModel
@@ -23,25 +32,100 @@ import at.luki0606.beerney.views.beerMap.BeerMap
 import at.luki0606.beerney.views.findHome.FindHome
 import at.luki0606.beerney.views.navigation.NavigationBar
 import at.luki0606.beerney.views.statistics.Statistics
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : ComponentActivity() {
     private val beerListViewModel: BeerListViewModel by viewModels()
-
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var currentLocation: Location
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //https://www.youtube.com/watch?v=mwzKYIB9cQs - code from here
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getCurrentLocation()
         setContent {
             BeerneyTheme {
-                BuildView(beerListViewModel)
+                BuildView(beerListViewModel, currentLocation)
+            }
+        }
+    }
+
+    private fun getCurrentLocation() {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
+                    val location: Location? = task.result
+                    print(location.toString())
+                    if (location == null) {
+                        Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show()
+                    } else {
+                        currentLocation = location
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please enable your location service", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+
+    private fun isLocationEnabled(): Boolean{
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            Companion.PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            } else {
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
 }
 
+
+
 @Composable
-fun BuildView(beerListViewModel: BeerListViewModel){
+fun BuildView(beerListViewModel: BeerListViewModel, currentLocation: Location){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .background(Alabaster),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
@@ -55,7 +139,7 @@ fun BuildView(beerListViewModel: BeerListViewModel){
                 0 -> BeerMap()
                 1 -> BeerList(beerListViewModel)
                 2 -> Statistics()
-                3 -> FindHome()
+                3 -> FindHome(currentLocation)
                 else -> BeerMap()
             }
         }
--- Beerney/app/src/main/java/at/luki0606/beerney/views/findHome/FindHome.kt
+++ Beerney/app/src/main/java/at/luki0606/beerney/views/findHome/FindHome.kt
@@ -1,16 +1,97 @@
 package at.luki0606.beerney.views.findHome
 
+import android.location.Location
+import android.location.LocationManager
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Path
+import at.luki0606.beerney.ui.theme.Ebony
+import kotlin.math.absoluteValue
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
-@Preview(showBackground = true)
 @Composable
-fun Preview() {
-    FindHome()
+fun FindHome(currentLocation: Location){
+    var bearing by remember { mutableFloatStateOf(0f) }
+
+    var targetPosition = Location(LocationManager.GPS_PROVIDER)
+
+    targetPosition.latitude = 47.568458
+    targetPosition.longitude = 14.866003
+
+    bearing = currentLocation.bearingTo(targetPosition)
+    Text(text = bearing.absoluteValue.toString())
+    ArrowComposable(bearing = bearing)
 }
 
 @Composable
-fun FindHome(){
-    Text(text = "FindHome")
+fun ArrowComposable(bearing: Float) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            // Mittelpunkt des Canvas
+            val centerX = canvasWidth / 2
+            val centerY = canvasHeight / 2
+
+            val arrowLength = min(canvasWidth, canvasHeight) * 0.4f
+            val arrowWidth = 20f
+            val arrowColor = Ebony
+
+            // Drehung des Pfeils basierend auf dem Bearing
+            val arrowRotation = Math.toRadians(90.0 - bearing.toDouble()).toFloat()
+
+            // Pfeilkörper zeichnen
+            drawLine(
+                color = arrowColor,
+                start = Offset(centerX, centerY),
+                end = Offset(
+                    centerX + arrowLength * cos(arrowRotation),
+                    centerY - arrowLength * sin(arrowRotation)
+                ),
+                strokeWidth = arrowWidth
+            )
+
+            // Pfeilkopf zeichnen
+            val headLength = arrowLength * 0.2f
+            val headWidth = arrowWidth * 2
+            val headOffsetX = centerX + arrowLength * cos(arrowRotation)
+            val headOffsetY = centerY - arrowLength * sin(arrowRotation)
+
+            val headBase = Offset(
+                headOffsetX + headLength * cos(arrowRotation + Math.toRadians(165.0).toFloat()),
+                headOffsetY - headLength * sin(arrowRotation + Math.toRadians(165.0).toFloat())
+            )
+
+            val headTip = Offset(
+                headOffsetX + headLength * cos(arrowRotation - Math.toRadians(165.0).toFloat()),
+                headOffsetY - headLength * sin(arrowRotation - Math.toRadians(165.0).toFloat())
+            )
+
+            drawPath(
+                path = Path().apply {
+                    moveTo(headOffsetX, headOffsetY)
+                    lineTo(headBase.x, headBase.y)
+                    lineTo(headTip.x, headTip.y)
+                    close()
+                },
+                color = arrowColor
+            )
+        }
+    }
 }
